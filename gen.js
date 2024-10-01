b22566cd60ca846e8e914dffaa1df5b24f4c1b87
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-09-30 20:40:25 -0700

    Create gen.js

---
+++ gen.js
@@ -0,0 +1,41 @@
+const files = {
+	wav: [
+		"83a9787d4cb6f3b7632b4ddfebf74367.wav",
+		"0c4c4fdb048e7d4d2f8c67a9c3d85af0.wav",
+		"2fdbf5af3fe9ce2b05787c9fee63e56a.wav",
+		"959036cb4689ea2929a49ac03fce5e95.wav",
+		"af5ab2a540e9403268e67637f595e887.wav",
+		"c0d91e60caf164fe4bf4610f092a52b4.wav",
+		"e93976eccf90624ceef39d80ebee8fc8.wav",
+		"c3fae7c1ddde6532bf2b9a6192caed13.wav",
+		"04c74a7f3c20a0b6e0209cc07613bae0.wav",
+		"4fc1905664d2b80ad2f24b5c7ed83cd5.wav",
+		"ade5cf8947606656488bdc8060c714f1.wav",
+		"bf50ac26e8f6df270819bf9008919582.wav",
+		"1ddf1a8893b568f9e6e9b640f3c54e02.wav",
+		"9f1db362aabec8aba8fe31f93209abd2.wav",
+		"73177387ce2d74655243380b6bd0bf4e.wav",
+		"ad0bb13addd1c61168af3521bfc07342.wav",
+		"9dd35c03fb9d76be2bab1f5c2e05df8b.wav",
+		"cf4fa2caf6d89b079d2fc5b6524e6143.wav",
+		"0e3727551fab5a8b1c0d3eda39205bc8.wav",
+		"a97c4d5a4e41f2ab56d4e698023b834c.wav"
+	],
+	png: [
+		"2e19abeec2f060432d234346f1a88e8d.png",
+		"014e4b3cb45c77ac939ce20b790bfa1d.png",
+		"82b6da7446390dcd56842382dd791e5d.png",
+		"570368bf164fec9f6d331d8eb5976319.png",
+		"a60acdcd0469b3463c648f24f853245d.png",
+		"e6face0567e4ced1cef6b801da375aaf.png",
+		"827785f559389602b96e94cf50e8699f.png",
+		"2de8b22d2c99e0fd97f388af50adf9b5.png",
+		"c98ce7d3be638326d790b91b5a4ed035.png",
+		"29ba1e881f6da473cbdfeef4d90611be.png"
+	]
+}
+const AudioGeneration = {
+	static: function() {
+		
+	}
+}
